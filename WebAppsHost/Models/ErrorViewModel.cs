ffa10499a64f8d6bf8ef159d344592cdc5b84c41
Senthilkumar SV <sveeindia@gmail.com>
2020-08-26 14:07:50 +0100

    Initial Commit

---
+++ WebAppsHost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppsHost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
